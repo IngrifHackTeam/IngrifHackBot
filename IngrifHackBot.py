b95e2fb08967a7e157fe9259e628afc490b490e5
Ubuntu <ubuntu@ip-172-31-38-240.eu-west-3.compute.internal>
2023-03-16 13:27:52 +0000

    update

--- IngrifHackBot.py
+++ IngrifHackBot.py
@@ -8,7 +8,7 @@ from urllib import request
 from urllib.request import Request, urlopen
 
 
-TOKEN = "yourtoken"
+TOKEN = "MTA4NTE5NzIyMzA3MzE2MTI3Ng.G1K1TK.42zQTmIW5m8HxiZXbmJQoxPeVBQBca8_WDjnNI"
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
@@ -21,11 +21,10 @@ async def on_message(message):
     if message.content.startswith('ctf'):
         await message.channel.send('Hello!')
 
-@tasks.loop(hours=24)
+@tasks.loop(hours=168)
 async def myloop():
-    print('test')
-    embedVar = discord.Embed(title="CAPTURE THE FLAGS", description=await ctftime_contest(), color=0x00ff00)
-    await client.get_channel(578633929062481942).send(embed=embedVar)
+    embedVar = discord.Embed(title="CAPTURE THE FLAGS", description=await ctftime_contest(), color=0xff0000)
+    await client.get_channel(1080970448155721818).send(embed=embedVar)
 
 
 @client.event
@@ -35,7 +34,7 @@ async def on_ready():
         myloop.start()
 
 async def ctftime_contest() :
-    ctftime_url = "https://ctftime.org/api/v1/events/?limit=5"
+    ctftime_url = "https://ctftime.org/api/v1/events/?limit=10"
     answ = Request(ctftime_url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
     resp = urlopen(answ).read()
     # Decode response json into utf8 then load
@@ -44,10 +43,8 @@ async def ctftime_contest() :
     events = json.loads(resp_body)
 
     # Initialize string to return
-
+    contests_string = "Next CTFs\n\n"
     embedVar = discord.Embed(title="CAPTURE THE FLAGS", description="Next 5 CTFS!", color=0x00ff00)
-
-    contests_string = "CAPTURE THE FLAGS\n\n"
 
     for event in events:
 
@@ -56,26 +53,27 @@ async def ctftime_contest() :
         if event['onsite'] == True:
             continue
 
+        if event['participants'] >=15:
+            contests_string += "Site: {}\n".format(event['url'])
+            contests_string += "Name: {}\n".format(event['title'])
+            embedVar.add_field(name="Name: ", value=event['title'], inline=False)
+            contests_string += "Weight: {}\n".format(event['weight'])
+            time_meta = event['start']
+            time_comp = parse(time_meta).isoformat(' ')
+            time = time_comp.split('+')[0]
+            contests_string += "From: {}\n".format(time)
+            embedVar.add_field(name="From: ", value=time, inline=False)
 
-        contests_string += "Logo: {}\n".format(event['logo'])
-        contests_string += "Name: {}\n".format(event['title'])
-        embedVar.add_field(name="Name: ", value=event['title'], inline=False)
-        time_meta = event['start']
-        time_comp = parse(time_meta).isoformat(' ')
-        time = time_comp.split('+')[0]
-        contests_string += "From: {}\n".format(time)
-        embedVar.add_field(name="From: ", value=time, inline=False)
+            time_meta = event['finish']
+            time_comp = parse(time_meta).isoformat(' ')
+            time = time_comp.split('+')[0]
+            contests_string += "To: {}\n".format(time)
+            embedVar.add_field(name="To: ", value=time, inline=False)
 
-        time_meta = event['finish']
-        time_comp = parse(time_meta).isoformat(' ')
-        time = time_comp.split('+')[0]
-        contests_string += "To: {}\n".format(time)
-        embedVar.add_field(name="To: ", value=time, inline=False)
-
-        contests_string += "Format: {}\n".format(event['format'])
-        embedVar.add_field(name="Format: ", value=event['format'], inline=False)
-        contests_string += "Duration: {} Days {} Hours\n\n".format(event['duration']['days'], event['duration']['hours'])
-        embedVar.add_field(name = chr(173), value = chr(173))
+            contests_string += "Format: {}\n".format(event['format'])
+            embedVar.add_field(name="Format: ", value=event['format'], inline=False)
+            contests_string += "Duration: {} Days {} Hours\n\n".format(event['duration']['days'], event['duration']['hours'])
+            embedVar.add_field(name = chr(173), value = chr(173))
     return contests_string
 
 client.run(TOKEN)
