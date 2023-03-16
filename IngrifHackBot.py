c95c03b0bae586a4bb583dc5ce6dd3acb094e428
Iraton <notari.filippo@outlook.it>
2023-03-16 20:43:00 +0100

    Update IngrifHackBot.py

--- IngrifHackBot.py
+++ IngrifHackBot.py
@@ -36,7 +36,7 @@ async def on_ready():
         myloop.start()
 
 async def ctftime_contest() :
-    ctftime_url = "https://ctftime.org/api/v1/events/?limit=20"
+    ctftime_url = "https://ctftime.org/api/v1/events/?limit=10"
     answ = Request(ctftime_url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
     resp = urlopen(answ).read()
     # Decode response json into utf8 then load
